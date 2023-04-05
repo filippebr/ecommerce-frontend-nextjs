7b97cef388b8b374e3ec06a4032aa64d02b69253
Filippe Braga <filippeffx@hotmail.com>
2023-04-04 21:30:08 -0300

    installed redux

--- src/__tests__/Home.test.tsx
+++ src/__tests__/Home.test.tsx
@@ -1,12 +1,3 @@
-// import { test, expect } from '@playwright/test';
-
-// test('should navigate to the about page', async ({ page }) => {
-//   await page.goto('/');
-//   await page.click('text=About');
-//   await expect(page).toHaveURL('/about');
-//   await expect(page.locator('h1')).toContainText('Hello World!');
-// });
-
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
 
