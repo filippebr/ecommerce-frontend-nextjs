bef6fc0a75c021bc94d2a9dd5539c1d823c5494f
Filippe Braga <filippeffx@hotmail.com>
2023-04-06 19:59:29 -0300

    colors names

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -3,6 +3,25 @@ import React from 'react';
 export default function Head() {
   return (
     <>
+      <header className="bg-luckypoint">
+        <div className="w-full mx-auto py-3 px-6 sm:px-12 lg:px-24">
+          <div className="grid grid-cols-2 gap-4">
+            <div>
+              <p className="text-md text-hintofyellow">
+                Free Shipping Over $100 & Free Returns
+              </p>
+            </div>
+            <div className="text-right">
+              <p className="text-md text-hintofyellow">
+                Hotline:
+                <a className="text-spunpearl" href="tel: +91 8264954234">
+                  +91 8264954234
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
       <h1 className="text-3xl flex justify-center items-center">
         Hello from Head
       </h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,34 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['var(--font-rubik)'],
-      },
+    colors: {
+      white: '#fff',
+      black: '#000',
+      affair: '#71467f',
+      clairvoyant: '#460959',
+      eminence: '#5B266C',
+      trendypink: '#8C6E95',
+      amethystsmoke: '#AB9DB0',
+      contessa: '#BF6668',
+      darkburgundy: '#86090C',
+      stiletto: '#A23538',
+      petiteorchid: '#DFA2A4',
+      cinderella: '#FDE2E2',
+      eastbay: '#505485',
+      luckypoint: '#12185E',
+      jacarta: '#2F3471',
+      waterloo: '#777A9C',
+      spunpearl: '#A6A7B7',
+      wildwillow: '#B7BD65',
+      yukongold: '#7C8509',
+      sushi: '#99A134',
+      deco: '#D9DEA1',
+      hintofyellow: '#FAFCE0',
     },
+    fontFamily: {
+      sans: ['var(--font-rubik)'],
+    },
+    extend: {},
   },
   plugins: [],
 };
