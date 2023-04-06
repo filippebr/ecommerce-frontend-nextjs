a6eca51b58f85fea20c9ac380f82e21270f48064
Filippe Braga <filippeffx@hotmail.com>
2023-04-05 22:24:27 -0300

    head and footer working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import Link from 'next/link';
+import Footer from '@/components/Footer';
 
 export default function Home() {
   return (
-    <h1 className="text-3xl flex justify-center items-center">Hello World!</h1>
+    <>
+      <h1 className="text-3xl flex justify-center items-center">
+        Hello from Home!
+      </h1>
+      <Footer />
+    </>
   );
 }
