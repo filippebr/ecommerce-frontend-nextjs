9ee73110dbe0b57e4837d4aa70b26bb863e5adcb
Filippe Braga <filippeffx@hotmail.com>
2023-04-18 21:35:43 -0300

    initial footer

---
+++ src/views/Footer/index.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="py-3 bg-jacarta"></footer>
+      <footer className="py-3 bg-jacarta"></footer>
+      <footer className="py-3 bg-jacarta"></footer>
+    </>
+  );
+}
