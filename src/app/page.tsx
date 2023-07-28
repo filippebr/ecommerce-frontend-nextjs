9cecef70d75a3f7fe43f31e5143f0849ec8adca4
Filippe Braga <filippeffx@hotmail.com>
2023-07-27 21:43:22 -0300

    footer middle links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -16,5 +16,5 @@ export default function Home() {
         Contact
       </Link>
     </>
-  );
+  )
 }
--- src/components/FooterMiddle.tsx
+++ src/components/FooterMiddle.tsx
@@ -1,13 +1,32 @@
+import Link from 'next/link'
+
 export default function FooterMiddle() {
   return (
     <>
       <footer className="bg-jacarta py-4">
         <div className="w-full mx-auto py-3 px-6 sm:px-12 lg:px-24">
           <div className="grid grid-cols-12 items-center">
-            <div className="col-span-3 text-white">First Column</div>
-            <div className="col-span-3 text-white">Second Column</div>
-            <div className="col-span-3 text-white">Third Column</div>
-            <div className="col-span-3 text-white">Fourth Column</div>
+            <div className="col-span-4 text-white">
+              <h4 className="text-white mb-4">Contact Us</h4>
+              <div></div>
+            </div>
+            <div className="col-span-3 text-white">
+              <h4 className="text-white mb-4">Information</h4>
+              <div></div>
+            </div>
+            <div className="col-span-3 text-white">
+              <h4 className="text-white mb-4">Account</h4>
+              <div></div>
+            </div>
+            <div className="col-span-2 text-white">
+              <h4 className="text-white mb-4">Quick Links</h4>
+              <div className="">
+                <Link href="#">Laptops</Link>
+                <Link href="#">Headphones</Link>
+                <Link href="#">Tablets</Link>
+                <Link href="#">Watch</Link>
+              </div>
+            </div>
           </div>
         </div>
       </footer>
