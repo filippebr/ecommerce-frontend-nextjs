931eae57b27975571a04c9a321cb36a379465857
Filippe Braga <filippeffx@hotmail.com>
2023-07-27 22:24:15 -0300

    footer middle columns

--- src/components/FooterMiddle.tsx
+++ src/components/FooterMiddle.tsx
@@ -5,26 +5,66 @@ export default function FooterMiddle() {
     <>
       <footer className="bg-jacarta py-4">
         <div className="w-full mx-auto py-3 px-6 sm:px-12 lg:px-24">
-          <div className="grid grid-cols-12 items-center">
+          <div className="grid grid-cols-12 ">
             <div className="col-span-4 text-white">
-              <h4 className="text-white mb-4">Contact Us</h4>
-              <div></div>
+              <h4 className="text-white mb-4 text-xl">Contact Us</h4>
+              <div className="flex flex-col justify-center">
+                <address className="text-white text-xs">
+                  John Doe, 777 Near Ville Town, <br /> São Paulo, São Paulo
+                  <br />
+                  Pincode: 99999
+                </address>
+              </div>
             </div>
             <div className="col-span-3 text-white">
-              <h4 className="text-white mb-4">Information</h4>
-              <div></div>
+              <h4 className="text-white mb-4 text-xl">Information</h4>
+              <div className="flex flex-col justify-center">
+                <Link className="text-white text-sm py-1 mb-0" href="#">
+                  Privacy Policy
+                </Link>
+                <Link className="text-white text-sm py-1 mb-0" href="#">
+                  Refund Policy
+                </Link>
+                <Link className="text-white text-sm py-1 mb-0" href="#">
+                  Shipping Policy
+                </Link>
+                <Link className="text-white text-sm py-1 mb-0" href="#">
+                  Terms & Conditions
+                </Link>
+                <Link className="text-white text-sm py-1 mb-0" href="#">
+                  Blogs
+                </Link>
+              </div>
             </div>
             <div className="col-span-3 text-white">
-              <h4 className="text-white mb-4">Account</h4>
-              <div></div>
+              <h4 className="text-white mb-4 text-xl">Account</h4>
+              <div className="flex flex-col justify-center">
+                <Link className="text-white text-sm py-1 mb-0" href="#">
+                  About us
+                </Link>
+                <Link className="text-white text-sm py-1 mb-0" href="#">
+                  Faq
+                </Link>
+                <Link className="text-white text-sm py-1 mb-0" href="#">
+                  Contact
+                </Link>
+              </div>
             </div>
             <div className="col-span-2 text-white">
-              <h4 className="text-white mb-4">Quick Links</h4>
-              <div className="">
-                <Link href="#">Laptops</Link>
-                <Link href="#">Headphones</Link>
-                <Link href="#">Tablets</Link>
-                <Link href="#">Watch</Link>
+              <h4 className="text-white text-xl mb-4 flex">Quick Links</h4>
+              <div className="flex flex-col justify-center">
+                <Link className="text-white text-sm py-1 mb-0" href="#">
+                  Laptops
+                </Link>
+                <Link className="text-white text-sm py-1 mb-0" href="#">
+                  Headphones
+                </Link>
+                <Link className="text-white text-sm py-1 mb-0" href="#">
+                  Tablets
+                </Link>
+                <Link className="text-white text-sm py-1 mb-0" href="#">
+                  Watch
+                </Link>
               </div>
             </div>
           </div>
