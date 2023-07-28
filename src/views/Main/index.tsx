74d3eb555ad0eb7b4adc1350e212f2f876fb88be
Filippe Braga <filippeffx@hotmail.com>
2023-07-27 22:54:36 -0300

    Initial Main design

---
+++ src/views/Main/index.tsx
@@ -0,0 +1,15 @@
+const Main = () => {
+  return (
+    <>
+      <section>
+        <div className="py-5">
+          <div className="grid grid-cols-12 items-center px-6 sm:px-12 lg:px-24">
+            <div className="col-span-6">
+              <div className="p-3"></div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
