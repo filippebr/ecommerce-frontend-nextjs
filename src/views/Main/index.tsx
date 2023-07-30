77bf4f878ca91994006cd7641093e0c85cd3162a
Filippe Braga <filippeffx@hotmail.com>
2023-07-29 22:35:38 -0300

    Initial Main component style frontend

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,11 +1,20 @@
-const Main = () => {
+import Image from 'next/image'
+
+export default function Main() {
   return (
     <>
       <section>
         <div className="py-5">
           <div className="grid grid-cols-12 items-center px-6 sm:px-12 lg:px-24">
             <div className="col-span-6">
-              <div className="p-3"></div>
+              <div className="p-3">
+                <Image
+                  width="100"
+                  height="100"
+                  src="/images/main-banner.jpg"
+                  alt="main banner"
+                ></Image>
+              </div>
             </div>
           </div>
         </div>
