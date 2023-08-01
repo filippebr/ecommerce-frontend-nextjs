6dc98a9c6b44d1f34fd1f149713e4b2eafdf5741
Filippe Braga <filippeffx@hotmail.com>
2023-08-01 13:15:34 -0300

    smaller banners main design web

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -8,6 +8,7 @@ export default function Main() {
         <div className="py-5">
           <div className="grid grid-cols-4 grid-rows-2 gap-4 items-center px-6 sm:px-12 lg:px-24">
             <div className="col-span-2 row-span-2">
+
               <div className="relative">
                 <Image
                   width={0}
@@ -23,9 +24,9 @@ export default function Main() {
                     SUPERCHARGED FOR PROS
                   </h4>
                   <h5 className="mb-4 text-4xl font-semibold">
-                    iPad S13+ Pro
+                    IPad S13+ Pro
                   </h5>
-                  <p className="mb-4">From $999.00 or $41.62/mo</p>
+                  <p className="mb-4 text-sm font-semibold">From $999.00 or $41.62/mo</p>
                   <Link
                     href=""
                     className="rounded-3xl bg-jacarta px-6 py-3 text-xs font-semibold text-white hover:bg-luckypoint"
@@ -47,7 +48,7 @@ export default function Main() {
                   src="/images/catbanner-01.jpg"
                   alt="small banner"
                 ></Image>
-                <div className="absolute left-6 top-8">
+                <div className="absolute left-6 top-12">
                   <h4 className="mb-2 text-sm font-bold text-luckypoint">
                     BEST SALE
                   </h4>
@@ -55,12 +56,6 @@ export default function Main() {
                     Laptops Max
                   </h5>
                   <p className="mb-2 text-sm font-semibold">From $1699.00 <br />or $41.62/mo.</p>
-                  <Link
-                    href=""
-                    className="rounded-2xl bg-jacarta px-6 py-2 text-xs font-semibold text-white hover:bg-luckypoint"
-                  >
-                    BUY NOW
-                  </Link>
                 </div>
               </div>
             </div>
@@ -76,7 +71,7 @@ export default function Main() {
                   src="/images/catbanner-02.jpg"
                   alt="small banner"
                 ></Image>
-                <div className="absolute left-6 top-8">
+                <div className="absolute left-6 top-12">
                   <h4 className="mb-2 text-xs font-bold text-luckypoint">
                     15% OFF
                   </h4>
@@ -84,12 +79,6 @@ export default function Main() {
                     Smartwatch 7
                   </h5>
                   <p className="mb-2 text-sm font-semibold">Shop the latest band <br />styles and colors</p>
-                  <Link
-                    href=""
-                    className="rounded-2xl bg-jacarta px-6 py-2 text-xs font-semibold text-white hover:bg-luckypoint"
-                  >
-                    BUY NOW
-                  </Link>
                 </div>
               </div>
             </div>
@@ -105,7 +94,7 @@ export default function Main() {
                   src="/images/catbanner-03.jpg"
                   alt="small banner"
                 ></Image>
-                <div className="absolute left-6 top-8">
+                <div className="absolute left-6 top-12">
                   <h4 className="mb-2 text-xs font-bold text-luckypoint">
                     NEW ARRIVAL
                   </h4>
@@ -113,12 +102,6 @@ export default function Main() {
                     Buy IPad Air
                   </h5>
                   <p className="mb-2 text-sm font-semibold">From $599 or<br />$49.91/mo. for 12 mo. *</p>
-                  <Link
-                    href=""
-                    className="rounded-2xl bg-jacarta px-6 py-2 text-xs font-semibold text-white hover:bg-luckypoint"
-                  >
-                    BUY NOW
-                  </Link>
                 </div>
               </div>
             </div>
@@ -134,7 +117,7 @@ export default function Main() {
                   src="/images/catbanner-04.jpg"
                   alt="small banner"
                 ></Image>
-                <div className="absolute left-6 top-8">
+                <div className="absolute left-6 top-12">
                   <h4 className="mb-2 text-xs font-bold text-luckypoint">
                     FREE ENGRAVING
                   </h4>
@@ -142,12 +125,6 @@ export default function Main() {
                     AirPods Max
                   </h5>
                   <p className="mb-2 text-sm font-semibold">High-fidelity playback & <br /> ultra-low distortion</p>
-                  <Link
-                    href=""
-                    className="rounded-2xl bg-jacarta px-6 py-2 text-xs font-semibold text-white hover:bg-luckypoint"
-                  >
-                    BUY NOW
-                  </Link>
                 </div>
               </div>
             </div>
