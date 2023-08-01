c6c8e098104fb167c0d69d06a22ed7c3aaa32272
Filippe Braga <filippeffx@hotmail.com>
2023-08-01 13:02:28 -0300

    images smaller banners main web

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -23,9 +23,9 @@ export default function Main() {
                     SUPERCHARGED FOR PROS
                   </h4>
                   <h5 className="mb-4 text-4xl font-semibold">
-                    iPad S13+ Pro.
+                    iPad S13+ Pro
                   </h5>
-                  <p className="mb-4">From $999.00 or $41.62/mo.</p>
+                  <p className="mb-4">From $999.00 or $41.62/mo</p>
                   <Link
                     href=""
                     className="rounded-2xl bg-jacarta px-6 py-2 text-xs font-semibold text-white hover:bg-luckypoint"
@@ -48,13 +48,13 @@ export default function Main() {
                   alt="small banner"
                 ></Image>
                 <div className="absolute left-2 top-8">
-                  <h4 className="mb-2 font-bold text-luckypoint">
-                    SMALL IMAGE 01
+                  <h4 className="mb-2 text-sm font-bold text-luckypoint">
+                    BEST SALE
                   </h4>
                   <h5 className="mb-2 text-xl font-semibold">
-                    iPad S13+ Pro.
+                    Laptops Max
                   </h5>
-                  <p className="mb-2">From $999.00 or $41.62/mo.</p>
+                  <p className="mb-2 text-sm font-semibold">From $1699.00 <br />or $41.62/mo.</p>
                   <Link
                     href=""
                     className="rounded-2xl bg-jacarta px-6 py-2 text-xs font-semibold text-white hover:bg-luckypoint"
@@ -64,6 +64,7 @@ export default function Main() {
                 </div>
               </div>
             </div>
+
             <div className="col-span-1 row-span-1">
               <div className="relative">
                 <Image
@@ -76,13 +77,13 @@ export default function Main() {
                   alt="small banner"
                 ></Image>
                 <div className="absolute left-2 top-8">
-                  <h4 className="mb-2 font-bold text-luckypoint">
-                    SMALL IMAGE 01
+                  <h4 className="mb-2 text-xs font-bold text-luckypoint">
+                    NEW ARRIVAL
                   </h4>
                   <h5 className="mb-2 text-xl font-semibold">
-                    iPad S13+ Pro.
+                    Buy IPad Air
                   </h5>
-                  <p className="mb-2">From $999.00 or $41.62/mo.</p>
+                  <p className="mb-2 text-sm font-semibold">From $599.00 or <br />$49.91/mo.</p>
                   <Link
                     href=""
                     className="rounded-2xl bg-jacarta px-6 py-2 text-xs font-semibold text-white hover:bg-luckypoint"
@@ -92,6 +93,7 @@ export default function Main() {
                 </div>
               </div>
             </div>
+
             <div className="col-span-1 row-span-1">
               <div className="relative">
                 <Image
@@ -104,13 +106,13 @@ export default function Main() {
                   alt="small banner"
                 ></Image>
                 <div className="absolute left-2 top-8">
-                  <h4 className="mb-2 font-bold text-luckypoint">
-                    SMALL IMAGE 01
+                  <h4 className="mb-2 text-xs font-bold text-luckypoint">
+                    15% OFF
                   </h4>
                   <h5 className="mb-2 text-xl font-semibold">
-                    iPad S13+ Pro.
+                    Smartwatch 7
                   </h5>
-                  <p className="mb-2">From $999.00 or $41.62/mo.</p>
+                  <p className="mb-2 text-sm font-semibold">Shop the latest band<br /> styles and colors</p>
                   <Link
                     href=""
                     className="rounded-2xl bg-jacarta px-6 py-2 text-xs font-semibold text-white hover:bg-luckypoint"
@@ -120,6 +122,7 @@ export default function Main() {
                 </div>
               </div>
             </div>
+
             <div className="col-span-1 row-span-1">
               <div className="relative">
                 <Image
@@ -132,13 +135,13 @@ export default function Main() {
                   alt="small banner"
                 ></Image>
                 <div className="absolute left-2 top-8">
-                  <h4 className="mb-2 font-bold text-luckypoint">
-                    SMALL IMAGE 01
+                  <h4 className="mb-2 text-xs font-bold text-luckypoint">
+                    FREE ENGRAVING
                   </h4>
                   <h5 className="mb-2 text-xl font-semibold">
-                    iPad S13+ Pro.
+                    AirPods Max
                   </h5>
-                  <p className="mb-2">From $999.00 or $41.62/mo.</p>
+                  <p className="mb-2 text-sm font-semibold">High-fidelity playback & <br /> ultra-low distortion</p>
                   <Link
                     href=""
                     className="rounded-2xl bg-jacarta px-6 py-2 text-xs font-semibold text-white hover:bg-luckypoint"
