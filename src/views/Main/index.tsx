9819b40c9a5bb798e05c6c4b4003acf7c8f2910a
Filippe Braga <filippeffx@hotmail.com>
2023-08-01 18:22:09 -0300

    services design web

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -24,7 +24,7 @@ export default function Main() {
                     SUPERCHARGED FOR PROS
                   </h4>
                   <h5 className="mb-4 text-4xl font-semibold">
-                    IPad S13+ Pro
+                    iPad S13+ Pro
                   </h5>
                   <p className="mb-4 text-sm">From $999.00 or $41.62/mo</p>
                   <Link
@@ -129,6 +129,86 @@ export default function Main() {
               </div>
             </div>
 
+          </div>
+        </div>
+      </section>
+      <section className="py-5">
+        <div className="grid grid-cols-5 items-center px-2 sm:px-4 lg:px-8">
+
+          <div className="col-span-1 flex items-center gap-2">
+            <Image
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: 'auto', height: 'auto' }}
+              className="rounded-lg"
+              src="/images/service.png"
+              alt="service"
+            ></Image>
+            <div className="">
+              <h6 className="font-bold">Free Shipping</h6>
+              <p>From all orders over $5</p>
+            </div>
+          </div>
+          <div className="col-span-1 flex items-center gap-2">
+            <Image
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: 'auto', height: 'auto' }}
+              className="rounded-lg"
+              src="/images/service-02.png"
+              alt="service"
+            ></Image>
+            <div className="">
+              <h6 className="font-bold">Daily Surprise Offers</h6>
+              <p>Save up to 25% off</p>
+            </div>
+          </div>
+          <div className="col-span-1 flex items-center gap-2">
+            <Image
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: 'auto', height: 'auto' }}
+              className="rounded-lg"
+              src="/images/service-03.png"
+              alt="service"
+            ></Image>
+            <div className="">
+              <h6 className="font-bold">Support 24/7</h6>
+              <p>Shop with a expert</p>
+            </div>
+          </div>
+          <div className="col-span-1 flex items-center gap-2">
+            <Image
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: 'auto', height: 'auto' }}
+              className="rounded-lg"
+              src="/images/service-04.png"
+              alt="service"
+            ></Image>
+            <div className="">
+              <h6 className="font-bold">Affordable Prices</h6>
+              <p>Get Factory Default Price</p>
+            </div>
+          </div>
+          <div className="col-span-1 flex items-center gap-2">
+            <Image
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: 'auto', height: 'auto' }}
+              className="rounded-lg"
+              src="/images/service-05.png"
+              alt="service"
+            ></Image>
+            <div className="">
+              <h6 className="font-bold">Secure Payments</h6>
+              <p>100% Protected Payment</p>
+            </div>
           </div>
         </div>
       </section>
