05cf31c011c6b8edc6b4b178d921b3a8692c2732
Filippe Braga <filippeffx@hotmail.com>
2023-07-31 21:31:04 -0300

    image banners main design web

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -35,120 +35,120 @@ export default function Main() {
                 </div>
               </div>
             </div>
-            <div className="col-span-2 row-span-2">
-              <div className="grid grid-cols-12 gap-2">
 
-                <div className="col-span-6">
-                  <div className="relative">
-                    <Image
-                      width={0}
-                      height={0}
-                      sizes="100vw"
-                      style={{ width: '100%', height: 'auto' }}
-                      className="rounded-lg"
-                      src="/images/catbanner-01.jpg"
-                      alt="small banner"
-                    ></Image>
-                    <div className="absolute left-2 top-8">
-                      <h4 className="mb-2 font-bold text-luckypoint">
-                        SMALL IMAGE 01
-                      </h4>
-                      <h5 className="mb-2 text-xl font-semibold">
-                        iPad S13+ Pro.
-                      </h5>
-                      <p className="mb-2">From $999.00 or $41.62/mo.</p>
-                      <Link
-                        href=""
-                        className="rounded-2xl bg-jacarta px-6 py-2 text-xs font-semibold text-white hover:bg-luckypoint"
-                      >
-                        BUY NOW
-                      </Link>
-                    </div>
-                  </div>
-                  <div className="relative">
-                    <Image
-                      width={0}
-                      height={0}
-                      sizes="100vw"
-                      style={{ width: '100%', height: 'auto' }}
-                      className="rounded-lg"
-                      src="/images/catbanner-01.jpg"
-                      alt="small banner"
-                    ></Image>
-                    <div className="absolute left-4 top-8">
-                      <h4 className="mb-2 font-bold text-luckypoint">
-                        SMALL IMAGE 02
-                      </h4>
-                      <h5 className="mb-2 text-xl font-semibold">
-                        iPad S13+ Pro.
-                      </h5>
-                      <p className="mb-2">From $999.00 or $41.62/mo.</p>
-                      <Link
-                        href=""
-                        className="rounded-2xl bg-jacarta px-6 py-2 text-xs font-semibold text-white hover:bg-luckypoint"
-                      >
-                        BUY NOW
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col-span-6">
-                  <div className="relative">
-                    <Image
-                      width={0}
-                      height={0}
-                      sizes="100vw"
-                      style={{ width: '100%', height: 'auto' }}
-                      className="rounded-lg"
-                      src="/images/catbanner-01.jpg"
-                      alt="small banner"
-                    ></Image>
-                    <div className="absolute left-2 top-8">
-                      <h4 className="mb-2 font-bold text-luckypoint">
-                        SMALL IMAGE 03
-                      </h4>
-                      <h5 className="mb-2 text-xl font-semibold">
-                        iPad S13+ Pro.
-                      </h5>
-                      <p className="mb-2">From $999.00 or $41.62/mo.</p>
-                      <Link
-                        href=""
-                        className="rounded-2xl bg-jacarta px-6 py-2 text-xs font-semibold text-white hover:bg-luckypoint"
-                      >
-                        BUY NOW
-                      </Link>
-                    </div>
-                  </div>
-                  <div className="relative">
-                    <Image
-                      width={0}
-                      height={0}
-                      sizes="100vw"
-                      style={{ width: '100%', height: 'auto' }}
-                      className="rounded-lg"
-                      src="/images/catbanner-01.jpg"
-                      alt="small banner"
-                    ></Image>
-                    <div className="absolute left-4 top-8">
-                      <h4 className="mb-2 font-bold text-luckypoint">
-                        SMALL IMAGE 04
-                      </h4>
-                      <h5 className="mb-2 text-xl font-semibold">
-                        iPad S13+ Pro.
-                      </h5>
-                      <p className="mb-2">From $999.00 or $41.62/mo.</p>
-                      <Link
-                        href=""
-                        className="rounded-2xl bg-jacarta px-6 py-2 text-xs font-semibold text-white hover:bg-luckypoint"
-                      >
-                        BUY NOW
-                      </Link>
-                    </div>
-                  </div>
+            <div className="col-span-1 row-span-1">
+              <div className="relative">
+                <Image
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  style={{ width: '100%', height: 'auto' }}
+                  className="rounded-lg"
+                  src="/images/catbanner-01.jpg"
+                  alt="small banner"
+                ></Image>
+                <div className="absolute left-2 top-8">
+                  <h4 className="mb-2 font-bold text-luckypoint">
+                    SMALL IMAGE 01
+                  </h4>
+                  <h5 className="mb-2 text-xl font-semibold">
+                    iPad S13+ Pro.
+                  </h5>
+                  <p className="mb-2">From $999.00 or $41.62/mo.</p>
+                  <Link
+                    href=""
+                    className="rounded-2xl bg-jacarta px-6 py-2 text-xs font-semibold text-white hover:bg-luckypoint"
+                  >
+                    BUY NOW
+                  </Link>
                 </div>
               </div>
             </div>
+            <div className="col-span-1 row-span-1">
+              <div className="relative">
+                <Image
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  style={{ width: '100%', height: 'auto' }}
+                  className="rounded-lg"
+                  src="/images/catbanner-01.jpg"
+                  alt="small banner"
+                ></Image>
+                <div className="absolute left-2 top-8">
+                  <h4 className="mb-2 font-bold text-luckypoint">
+                    SMALL IMAGE 01
+                  </h4>
+                  <h5 className="mb-2 text-xl font-semibold">
+                    iPad S13+ Pro.
+                  </h5>
+                  <p className="mb-2">From $999.00 or $41.62/mo.</p>
+                  <Link
+                    href=""
+                    className="rounded-2xl bg-jacarta px-6 py-2 text-xs font-semibold text-white hover:bg-luckypoint"
+                  >
+                    BUY NOW
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-span-1 row-span-1">
+              <div className="relative">
+                <Image
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  style={{ width: '100%', height: 'auto' }}
+                  className="rounded-lg"
+                  src="/images/catbanner-01.jpg"
+                  alt="small banner"
+                ></Image>
+                <div className="absolute left-2 top-8">
+                  <h4 className="mb-2 font-bold text-luckypoint">
+                    SMALL IMAGE 01
+                  </h4>
+                  <h5 className="mb-2 text-xl font-semibold">
+                    iPad S13+ Pro.
+                  </h5>
+                  <p className="mb-2">From $999.00 or $41.62/mo.</p>
+                  <Link
+                    href=""
+                    className="rounded-2xl bg-jacarta px-6 py-2 text-xs font-semibold text-white hover:bg-luckypoint"
+                  >
+                    BUY NOW
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-span-1 row-span-1">
+              <div className="relative">
+                <Image
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  style={{ width: '100%', height: 'auto' }}
+                  className="rounded-lg"
+                  src="/images/catbanner-01.jpg"
+                  alt="small banner"
+                ></Image>
+                <div className="absolute left-2 top-8">
+                  <h4 className="mb-2 font-bold text-luckypoint">
+                    SMALL IMAGE 01
+                  </h4>
+                  <h5 className="mb-2 text-xl font-semibold">
+                    iPad S13+ Pro.
+                  </h5>
+                  <p className="mb-2">From $999.00 or $41.62/mo.</p>
+                  <Link
+                    href=""
+                    className="rounded-2xl bg-jacarta px-6 py-2 text-xs font-semibold text-white hover:bg-luckypoint"
+                  >
+                    BUY NOW
+                  </Link>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </section>
