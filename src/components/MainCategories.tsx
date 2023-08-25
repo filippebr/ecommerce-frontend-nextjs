e6117256e44774a596abfc337b8568ede5ad091b
Filippe Braga <filippeffx@hotmail.com>
2023-08-25 20:32:42 -0300

    main categories data

--- src/components/MainCategories.tsx
+++ src/components/MainCategories.tsx
@@ -1,169 +1,31 @@
+import mainCategoriesData from "@/data/mainCategoriesData"
 import Image from "next/image"
 
 export default function MainCategories() {
   return (
     <section className="py-5">
       <div className="grid grid-cols-5 grid-rows-2 items-center px-4 sm:px-8 lg:px-16">
-        <div className="col-span-1 col-row-1 flex items-center px-2 justify-between">
-          <div className="">
-            <h6 className="font-bold">Computers and Laptop</h6>
-            <p>8 items</p>
-          </div>
-          <Image
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: 'auto', height: 'auto' }}
-            className="rounded-lg"
-            src="/images/laptop.jpg"
-            alt="service"
-          ></Image>
-        </div>
 
-        <div className="col-span-1 col-row-1 flex items-center justify-between px-2">
-          <div className="">
-            <h6 className="font-bold">Cameras & Videos</h6>
-            <p>8 items</p>
+        {mainCategoriesData.map((data) => (
+          <div key={data.id} className="col-span-1 col-row-1 flex items-center px-2 justify-between">
+            <div className="">
+              <h6 className="font-bold">{data.title}</h6>
+              <p>{data.quantity}</p>
+            </div>
+            <Image
+              width={0}
+              height={0}
+              sizes="100vw"
+              style={{ width: 'auto', height: 'auto' }}
+              className="rounded-lg"
+              src={data.src}
+              alt="service"
+            ></Image>
           </div>
-          <Image
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: 'auto', height: 'auto' }}
-            className="rounded-lg"
-            src="/images/camera.jpg"
-            alt="service"
-          ></Image>
-        </div>
+        ))}
 
-        <div className="col-span-1 col-row-1 flex items-center justify-between px-2">
-          <div className="">
-            <h6 className="font-bold">Smart Television</h6>
-            <p>12 items</p>
-          </div>
-          <Image
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: 'auto', height: 'auto' }}
-            className="rounded-lg"
-            src="/images/tv.jpg"
-            alt="service"
-          ></Image>
-        </div>
-
-        <div className="col-span-1 col-row-1 flex items-center justify-between px-2">
-          <div className="">
-            <h6 className="font-bold">Smartwatches</h6>
-            <p>8 items</p>
-          </div>
-          <Image
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: 'auto', height: 'auto' }}
-            className="rounded-lg"
-            src="/images/smartwatch.jpg"
-            alt="service"
-          ></Image>
-        </div>
-
-        <div className="col-span-1 col-row-1 flex items-center justify-between px-2">
-          <div className="">
-            <h6 className="font-bold">Music & Gaming</h6>
-            <p>4 items</p>
-          </div>
-          <Image
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: 'auto', height: 'auto' }}
-            className="rounded-lg"
-            src="/images/gaming.jpg"
-            alt="service"
-          ></Image>
-        </div>
-
-        <div className="col-span-1 col-row-1 flex items-center justify-between px-2">
-          <div className="">
-            <h6 className="font-bold">Mobiles & Tablets</h6>
-            <p>4 items</p>
-          </div>
-          <Image
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: 'auto', height: 'auto' }}
-            className="rounded-lg"
-            src="/images/mobile.jpg"
-            alt="service"
-          ></Image>
-        </div>
-
-        <div className="col-span-1 col-row-1 flex items-center justify-between px-2">
-          <div className="">
-            <h6 className="font-bold">Headphones</h6>
-            <p>4 items</p>
-          </div>
-          <Image
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: 'auto', height: 'auto' }}
-            className="rounded-lg"
-            src="/images/headphone.jpg"
-            alt="service"
-          ></Image>
-        </div>
-
-        <div className="col-span-1 col-row-1 flex items-center justify-between px-2">
-          <div className="">
-            <h6 className="font-bold">Accessories</h6>
-            <p>4 items</p>
-          </div>
-          <Image
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: 'auto', height: 'auto' }}
-            className="rounded-lg"
-            src="/images/acc.jpg"
-            alt="service"
-          ></Image>
-        </div>
-
-        <div className="col-span-1 col-row-1 flex items-center justify-between px-2">
-          <div className="">
-            <h6 className="font-bold">Portable Speakers</h6>
-            <p>4 items</p>
-          </div>
-          <Image
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: 'auto', height: 'auto' }}
-            className="rounded-lg"
-            src="/images/speaker.jpg"
-            alt="service"
-          ></Image>
-        </div>
-
-        <div className="col-span-1 col-row-1 flex items-center justify-between px-2">
-          <div className="">
-            <h6 className="font-bold">Home Appliances</h6>
-            <p>4 items</p>
-          </div>
-          <Image
-            width={0}
-            height={0}
-            sizes="100vw"
-            style={{ width: 'auto', height: 'auto' }}
-            className="rounded-lg"
-            src="/images/homeapp.jpg"
-            alt="service"
-          ></Image>
-        </div>
       </div>
+
     </section>
   )
 }
---
+++ src/data/mainCategoriesData.tsx
@@ -0,0 +1,64 @@
+const mainCategoriesData = [
+  {
+    id: 1,
+    title: 'Computers and Laptop',
+    quantity: '8 items',
+    src: '/images/laptop.jpg',
+  },
+  {
+    id: 2,
+    title: 'Cameras & Videos',
+    quantity: '12 items',
+    src: '/images/camera.jpg',
+  },
+  {
+    id: 3,
+    title: 'Smart Television',
+    quantity: '7 items',
+    src: '/images/tv.jpg'
+  },
+  {
+    id: 4,
+    title: 'Smartwatches',
+    quantity: '15 items',
+    src: '/images/smartwatches.jpg'
+  },
+  {
+    id: 5,
+    title: 'Music & Gaming',
+    quantity: '13 items',
+    src: '/images/gaming.jpg'
+  },
+  {
+    id: 6,
+    title: 'Mobiles & Tablets',
+    quantity: '17 items',
+    src: '/images/mobile.jpg'
+  },
+  {
+    id: 7,
+    title: 'Headphones',
+    quantity: '19 items',
+    src: '/images/headphones.jpg'
+  },
+  {
+    id: 8,
+    title: 'Accessories',
+    quantity: '17 items',
+    src: '/images/acc.jpg'
+  },
+  {
+    id: 9,
+    title: 'Portable Speakers',
+    quantity: '11 items',
+    src: '/images/speaker.jpg'
+  },
+  {
+    id: 10,
+    title: 'Home Appliances',
+    quantity: '9 items',
+    src: '/images/homeapp.jpg'
+  },
+]
+
+export default mainCategoriesData
