1516333470eaa22cadfdad9e10f53410bbc69c11
Filippe Braga <filippeffx@hotmail.com>
2023-09-29 23:48:05 -0300

    images marquee gradient

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -12,8 +12,8 @@ export default function Main() {
       <MainCategories />
       <section className="py-5">
         <div className="grid grid-cols-1 items-center px-4 sm:px-8 lg:px-16">
-          <div className="marquee-gradient bg-white p-4 shadow-marquee">
-            <Marquee className="flex flex-col">
+          <div className="bg-white p-4 shadow-marquee">
+            <Marquee className="flex flex-col" gradient={true} gradientColor={[248, 250, 253]} gradientWidth={300} speed={30}>
               <div className="mx-8 w-28">
                 <Image
                   width={200}
@@ -97,7 +97,7 @@ export default function Main() {
             </Marquee>
           </div>
         </div>
-      </section>
+      </section >
     </>
   )
 }
