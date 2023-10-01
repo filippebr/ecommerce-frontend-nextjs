3ee2a4c080865ec35386a17df95dad29cde771fb
Filippe Braga <filippeffx@hotmail.com>
2023-09-30 21:33:49 -0300

    main marquee data

---
+++ src/data/mainMarqueeData.tsx
@@ -0,0 +1,36 @@
+const mainMarqueeData = [
+  {
+    id: 0,
+    src: "/images/brand-01.png",
+  },
+  {
+    id: 1,
+    src: "/images/brand-02.png",
+  },
+  {
+    id: 2,
+    src: "/images/brand-03.png",
+  },
+  {
+    id: 3,
+    src: "/images/brand-04.png",
+  },
+  {
+    id: 4,
+    src: "/images/brand-05.png",
+  },
+  {
+    id: 5,
+    src: "/images/brand-06.png",
+  },
+  {
+    id: 6,
+    src: "/images/brand-07.png",
+  },
+  {
+    id: 7,
+    src: "/images/brand-08.png",
+  },
+]
+
+export default mainMarqueeData
