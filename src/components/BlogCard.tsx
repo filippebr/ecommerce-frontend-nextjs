41ef468f78be9efa055f3f318c47c9012915b27e
Filippe Braga <filippeffx@hotmail.com>
2023-10-02 21:09:26 -0300

    rocketseat config eslint

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function BlogCard() {
   return (
     <div className="col-span-1">
-      <div className="bg-white rounded-lg col-span-1">
+      <div className="flex flex-col items-center rounded-lg ">
         <Image
           width={0}
           height={0}
@@ -13,27 +13,26 @@ export default function BlogCard() {
           src="/images/blog-1.jpg"
           alt="blog"
           className="rounded-tl-lg rounded-tr-lg"
-        >
-        </Image>
+        ></Image>
         <div className="p-4">
           <p className="text-sm text-lightgray">01 Sep, 2023</p>
           <h5 className="title text-xl font-bold">
             A beautiful sunday morning renaissance
           </h5>
-          <p className="text-sm leading-6 text-lightgray ">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi, id ab. Perspiciatis culpa
-            iste quia commodi quibusdam, provident animi excepturi! Atque cumque et nulla porro illum quo,
-            similique itaque fuga?
+          <p className="mb-2 text-sm leading-6 text-lightgray">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi,
+            id ab. Perspiciatis culpa iste quia commodi quibusdam, provident
+            animi excepturi! Atque cumque et nulla porro illum quo, similique
+            itaque fuga?
           </p>
           <Link
             href="/"
-            className="bg-jacarta rounded-3xl text-lg text-white py-2 px-4"
+            className="rounded-3xl bg-jacarta px-4 py-2 text-lg text-white"
           >
             Read More
           </Link>
         </div>
       </div>
     </div>
-
   )
 }
--- src/components/MainMarquee.tsx
+++ src/components/MainMarquee.tsx
@@ -7,7 +7,13 @@ export default function MainMarquee() {
     <section className="py-5">
       <div className="grid grid-cols-1 items-center px-4 sm:px-8 lg:px-16">
         <div className="bg-white p-4 shadow-marquee">
-          <Marquee className="flex flex-col" gradient={true} gradientColor={[248, 250, 253]} gradientWidth={300} speed={30}>
+          <Marquee
+            className="flex flex-col"
+            gradient={true}
+            gradientColor={[248, 250, 253]}
+            gradientWidth={300}
+            speed={30}
+          >
             {mainMarqueeData.map((data) => (
               <div key={data.id} className="mx-8 w-28">
                 <Image
@@ -17,13 +23,12 @@ export default function MainMarquee() {
                   style={{ width: 'auto', height: 'auto' }}
                   src={data.src}
                   alt="brand"
-                >
-                </Image>
+                ></Image>
               </div>
             ))}
           </Marquee>
         </div>
       </div>
-    </section >
+    </section>
   )
 }
