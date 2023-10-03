89b53eae675650ba553e1f39cc280735628289c4
Filippe Braga <filippeffx@hotmail.com>
2023-10-03 00:30:55 -0300

    uncomment components

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,14 +1,17 @@
 // import MainBanner from "@/components/MainBanners"
 // import MainCategories from '@/components/MainCategories'
 import BlogCard from '@/components/BlogCard'
+import MainBanner from '@/components/MainBanners'
+import MainCategories from '@/components/MainCategories'
 import MainMarquee from '@/components/MainMarquee'
+import MainServices from '@/components/MainServices'
 
 export default function Main() {
   return (
     <>
-      {/* <MainBanner />
-      <MainServices /> */}
-      {/* <MainCategories /> */}
+      <MainBanner />
+      <MainServices />
+      <MainCategories />
       <MainMarquee />
       <section className="py-5">
         <div className="grid grid-cols-3 items-center justify-center px-4 text-3xl sm:px-8 lg:px-16">
