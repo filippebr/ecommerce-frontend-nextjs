69536c83ff240e6bbe3bcf042f9a1a44ef76e708
Filippe Braga <filippeffx@hotmail.com>
2023-10-06 20:10:24 -0300

    star rating product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -25,7 +25,7 @@ export default function ProductCard() {
             <h5 className="text-base text-darkgray">
               Kids headphones bulk 10 pack multi colored for students
             </h5>
-            <div className="flex flex-col">
+            <div className="grid grid-rows-1 grid-cols-5">
               {[...Array(5)].map((_, index) => (
                 <label className="" htmlFor="" key={index}>
                   <FaStar
