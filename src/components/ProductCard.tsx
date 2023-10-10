da7b411fa96372b39875d5d0c476b591bacf71d5
Filippe Braga <filippeffx@hotmail.com>
2023-10-09 23:35:06 -0300

    removed product details comment

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -45,40 +45,6 @@ export default function ProductCard() {
         </div>
         {/* Product Details */}
         <ProductDetails />
-        {/* <div className="space-y-4">
-          <h6 className="text-lg text-petiteorchid">Havels</h6>
-          <h5 className="text-base text-darkgray">
-            Kids headphones bulk 10 pack multi colored for students
-          </h5>
-          <div className="grid grid-cols-5">
-            <div className="flex justify-start">
-              {[...Array(5)].map((_, index) => (
-                <label htmlFor="" key={index}>
-                  <FaStar
-                    className={`${index <= selectedStarCount - 1 ? 'text-sushi' : ''
-                      } 
-                      cursor-pointer ${index + 1 <= selectedHoverStarCount ? 'text-sushi' : ''
-                      } `}
-                    onMouseOver={() => {
-                      setSelectedHoverStarCount(index + 1)
-                    }}
-                    onMouseOut={() => {
-                      setSelectedHoverStarCount(0)
-                    }}
-                    onClick={() => {
-                      setSelectedStarCount(index + 1)
-                    }}
-                    size={20}
-                  />
-                </label>
-              ))}
-            </div>
-          </div> 
-          <p>Rating Count: {selectedStarCount}</p>
-          <p>Hover Rating Count: {selectedHoverStarCount}</p> 
-          <p className="text-lg text-darkgray">$100.00</p>
-        </div> */}
-
         {/* Action Bar */}
         <div className="transition-right absolute right-[-30px] top-10 duration-300 ease-in-out group-hover:right-5">
           <div className="flex flex-col space-y-4">
