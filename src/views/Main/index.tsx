5102ad54d75d5a1a9a800b6530ad4ec0ba0a743c
Filippe Braga <filippeffx@hotmail.com>
2023-10-13 18:04:17 -0300

    special product component

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -20,16 +20,14 @@ export default function Main() {
           <ProductCard />
         </div>
       </section>
-      <section className="special-wrapper home-wrapper-2 py-5">
+      <section className="py-5">
         <div className="grid grid-cols-12 px-4 text-3xl sm:px-8 lg:px-16">
           <div className="col-span-12">
-            <div className="mb-8 text-2xl font-semibold">Special Products</div>
+            <h3 className="mb-8 text-2xl font-semibold">Special Products</h3>
           </div>
-          <div className="mb-8 text-2xl font-semibold">
-            <SpecialProduct />
-            <SpecialProduct />
-            <SpecialProduct />
-          </div>
+          <SpecialProduct />
+          <SpecialProduct />
+          <SpecialProduct />
         </div>
       </section>
       {/* <section className="py-5">
