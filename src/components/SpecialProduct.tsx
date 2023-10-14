c1232fc3b0d45d8eec921976672db80976f2646b
Filippe Braga <filippeffx@hotmail.com>
2023-10-13 21:04:48 -0300

    button cart special products components

--- src/components/SpecialProduct.tsx
+++ src/components/SpecialProduct.tsx
@@ -64,7 +64,7 @@ export default function SpecialProduct() {
                 <span className="py-1 px-3 rounded-full bg-contessa">1</span>
               </div>
             </div>
-            <div className="flex text-base space-y-4 mb-6">
+            <div className="flex flex-col text-base space-y-4 mb-6">
               <p>Products: 5</p>
               <div className="w-1/4 bg-lightgray rounded-full h-4 dark:bg-darkgray">
                 <div className="bg-waterloo h-4 rounded-full" style={{ width: "50%" }}></div>
@@ -72,7 +72,7 @@ export default function SpecialProduct() {
             </div>
             <Link
               href="/"
-              className="rounded-3xl bg-jacarta hover:bg-eastbay px-4 py-2 text-lg text-white"
+              className="rounded-3xl bg-jacarta hover:bg-eastbay px-4 py-2 text-lg text-white mt-6"
             >
               Add to Cart
             </Link>
