7a07909e8a3f679dd28fe24561417eaffa38d662
Filippe Braga <filippeffx@hotmail.com>
2023-10-17 23:34:54 -0300

    notebook popular products

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -49,6 +49,30 @@ export default function Main() {
               </div>
             </div>
           </div>
+          <div className="col-span-3">
+            <div className="relative mb-8 font-semibold">
+              <div className="text-black">
+                <div className="absolute left-8 top-8 space-y-3 bg-white">
+                  <h5 className="text-xs font-light">STUDIO DISPLAY</h5>
+                  <h6 className="text-2xl">600 nits of brightness.</h6>
+                  <p className="text-sm font-light">
+                    27-inch 5K Retina Display
+                  </p>
+                </div>
+                <Image
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  style={{
+                    width: 'auto',
+                    height: 'auto',
+                  }}
+                  src="/images/notebook.jpg"
+                  alt="notebook"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </section>
       <section className="py-5">
