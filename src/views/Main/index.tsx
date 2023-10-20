98f4c379eea5b71249a9ab484be68963447b89eb
Filippe Braga <filippeffx@hotmail.com>
2023-10-20 00:05:15 -0300

    uncomment main components

--- src/views/Main/index.tsx
+++ src/views/Main/index.tsx
@@ -1,7 +1,4 @@
 import MainBanner from '@/components/MainBanners'
-import MainCategories from '@/components/MainCategories'
-import MainMarquee from '@/components/MainMarquee'
-import MainServices from '@/components/MainServices'
 import ProductCard from '@/components/ProductCard'
 import SpecialProduct from '@/components/SpecialProduct'
 import Image from 'next/image'
@@ -10,9 +7,9 @@ export default function Main() {
   return (
     <>
       <MainBanner />
-      <MainServices />
-      <MainCategories />
-      <MainMarquee />
+      {/* <MainServices /> */}
+      {/* <MainCategories /> */}
+      {/* <MainMarquee /> */}
       <section className="py-5">
         <div className="grid grid-cols-12 px-4 text-3xl sm:px-8 lg:px-16">
           <div className="col-span-12">
