68b898cc01a08d9788bee182cac175b756caf57e
Filippe Braga <filippeffx@hotmail.com>
2023-10-20 00:07:29 -0300

    store page component

---
+++ src/app/store/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function Contact() {
+  return (
+    <div className="flex flex-col items-center justify-center text-3xl">
+      <Link href="/">Main</Link>
+      Hello from Store
+    </div>
+  )
+}
