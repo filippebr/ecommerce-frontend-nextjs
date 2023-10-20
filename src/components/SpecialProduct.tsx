01822d368b3c06022e7a063f9806fe7587444297
Filippe Braga <filippeffx@hotmail.com>
2023-10-20 18:36:06 -0300

    removed comments special products

--- src/components/SpecialProduct.tsx
+++ src/components/SpecialProduct.tsx
@@ -5,8 +5,6 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 export default function SpecialProduct() {
-  // const [selectedStarCount, setSelectedStarCount] = useState(0)
-  // const [selectedHoverStarCount, setSelectedHoverStarCount] = useState(0)
 
   return (
     <div className="col-span-4">
@@ -30,25 +28,6 @@ export default function SpecialProduct() {
             <div className="grid grid-cols-5">
               <div className="flex justify-start">
                 <RatingStar />
-                {/* {[...Array(5)].map((_, index) => (
-                  <label htmlFor="" key={index}>
-                    <FaStar
-                      className={`${index <= selectedStarCount - 1 ? 'text-sushi' : ''
-                        } cursor-pointer ${index + 1 <= selectedHoverStarCount ? 'text-sushi' : ''
-                        }`}
-                      onMouseOver={() => {
-                        setSelectedHoverStarCount(index + 1)
-                      }}
-                      onMouseOut={() => {
-                        setSelectedHoverStarCount(0)
-                      }}
-                      onClick={() => {
-                        setSelectedStarCount(index + 1)
-                      }}
-                      size={20}
-                    />
-                  </label>
-                ))} */}
               </div>
             </div>
             <p className="text-lg text-darkgray">
