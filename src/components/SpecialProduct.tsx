8fec9f3dfe0ddb8e55c7b4dae54fa3f957063c4e
Filippe Braga <filippeffx@hotmail.com>
2023-10-20 19:29:50 -0300

    rating star map special product component

--- src/components/SpecialProduct.tsx
+++ src/components/SpecialProduct.tsx
@@ -17,7 +17,7 @@ export default function SpecialProduct() {
               style={{ width: 'auto', height: 'auto' }}
               src="/images/watch.jpg"
               alt="watch"
-            ></Image>
+            />
           </div>
           <div className="space-y-4">
             <h5 className="text-lg text-petiteorchid font-semibold">Havels</h5>
@@ -26,8 +26,26 @@ export default function SpecialProduct() {
             </h6>
             <div className="grid grid-cols-5">
               <div className="flex justify-start">
-                <RatingStar></RatingStar>
-                { }
+                <RatingStar />
+                {/* {[...Array(5)].map((_, index) => (
+                  <label htmlFor="" key={index}>
+                    <FaStar
+                      className={`${index <= selectedStarCount - 1 ? 'text-sushi' : ''
+                        } cursor-pointer ${index + 1 <= selectedHoverStarCount ? 'text-sushi' : ''
+                        }`}
+                      onMouseOver={() => {
+                        setSelectedHoverStarCount(index + 1)
+                      }}
+                      onMouseOut={() => {
+                        setSelectedHoverStarCount(0)
+                      }}
+                      onClick={() => {
+                        setSelectedStarCount(index + 1)
+                      }}
+                      size={20}
+                    />
+                  </label>
+                ))} */}
               </div>
             </div>
             <p className="text-lg text-darkgray">
