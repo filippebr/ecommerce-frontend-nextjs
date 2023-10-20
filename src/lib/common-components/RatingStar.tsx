22965c4a0621adb62c664d058845a50d8b72b048
Filippe Braga <filippeffx@hotmail.com>
2023-10-20 17:59:01 -0300

    rating star separated component

---
+++ src/lib/common-components/RatingStar.tsx
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+'use client'
+import { useState } from 'react'
+import { FaStar } from 'react-icons/fa'
+
+export default function RatingStar() {
+  const [selectedStarCount, setSelectedStarCount] = useState(0)
+  const [selectedHoverStarCount, setSelectedHoverStarCount] = useState(0)
+
+  return (
+    <>
+      {[...Array(5)].map((_, index) => (
+        <label htmlFor="" key={index}>
+          <FaStar
+            className={`${index <= selectedStarCount - 1 ? 'text-sushi' : ''
+              } cursor-pointer ${index + 1 <= selectedHoverStarCount ? 'text-sushi' : ''
+              }`}
+            onMouseOver={() => {
+              setSelectedHoverStarCount(index + 1)
+            }}
+            onMouseOut={() => {
+              setSelectedHoverStarCount(0)
+            }}
+            onClick={() => {
+              setSelectedStarCount(index + 1)
+            }}
+            size={20}
+          />
+        </label>
+      ))}
+    </>
+  )
+}
