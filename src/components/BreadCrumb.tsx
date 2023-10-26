661ba7edd387eab1fa6856c5c99e67726a1bcc7a
Filippe Braga <filippeffx@hotmail.com>
2023-10-25 23:43:52 -0300

    breadcrumb text center

--- src/components/BreadCrumb.tsx
+++ src/components/BreadCrumb.tsx
@@ -9,7 +9,7 @@ export default function BreadCrumb({ title }: Props) {
     <div className="breadcrumb py-4">
       <div className="grid grid-cols-12 gap-4 px-4 text-3xl sm:px-8 lg:px-16">
         <div className="col-span-12">
-          <p>
+          <p className="flex items-center justify-center">
             <Link href="/" className="text-darkgray">
               Home
             </Link>{' '}
