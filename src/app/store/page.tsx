2b6b2fc691ce47f79c618a9618a6d195c76fa3c7
Filippe Braga <filippeffx@hotmail.com>
2023-10-26 22:16:22 -0300

    filter card styles our store view

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -6,7 +6,12 @@ export default function OurStore() {
       <BreadCrumb title="Our Store" />
       <div className="store-wrapper home-wrapper py-5">
         <div className="grid grid-cols-12 px-4 text-3xl sm:px-8 lg:px-16">
-          <div className="col-span-3"></div>
+          <div className="col-span-3">
+            <div className="rounded-lg bg-white px-3 py-4"></div>
+            <div className="rounded-lg bg-white px-3 py-4"></div>
+            <div className="rounded-lg bg-white px-3 py-4"></div>
+            <div className="rounded-lg bg-white px-3 py-4"></div>
+          </div>
           <div className="col-span-9"></div>
         </div>
       </div>
