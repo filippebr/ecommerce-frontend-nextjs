5448509675315975335c66a7fdcdea6c52105851
Filippe Braga <filippeffx@hotmail.com>
2023-10-26 22:30:56 -0300

    shop by categories list our store view

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -4,27 +4,35 @@ export default function OurStore() {
   return (
     <>
       <BreadCrumb title="Our Store" />
-      <div className="store-wrapper home-wrapper py-5">
+      <div className="py-5">
         <div className="grid grid-cols-12 px-4 text-3xl sm:px-8 lg:px-16">
           <div className="col-span-3">
             <div className="mb-1 rounded-lg bg-white px-3 py-4">
-              <h3 className="text-base font-semibold text-gray">
+              <h3 className="mb-5 text-base font-semibold text-gray">
                 Shop By Categories
+              </h3>
+              <div>
+                <ul>
+                  <li>Watch</li>
+                  <li>Tv</li>
+                  <li>Camera</li>
+                  <li>Laptop</li>
+                </ul>
+              </div>
+            </div>
+            <div className="mb-1 rounded-lg bg-white px-3 py-4">
+              <h3 className="mb-5 text-base font-semibold text-gray">
+                Filter by
               </h3>
             </div>
             <div className="mb-1 rounded-lg bg-white px-3 py-4">
-              <h3 className="text-base font-semibold text-gray">
-                Shop By Categories
+              <h3 className="mb-5 text-base font-semibold text-gray">
+                Product Tags
               </h3>
             </div>
             <div className="mb-1 rounded-lg bg-white px-3 py-4">
-              <h3 className="text-base font-semibold text-gray">
-                Shop By Categories
-              </h3>
-            </div>
-            <div className="mb-1 rounded-lg bg-white px-3 py-4">
-              <h3 className="text-base font-semibold text-gray">
-                Shop By Categories
+              <h3 className="mb-5 text-base font-semibold text-gray">
+                Random Product
               </h3>
             </div>
           </div>
