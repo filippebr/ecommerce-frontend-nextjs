be47984e5af5a6f1adc2425307e43fa0f4185257
Filippe Braga <filippeffx@hotmail.com>
2023-11-02 19:18:45 -0300

    size ham grid store data

--- src/data/hamburgerGridStoreData.tsx
+++ src/data/hamburgerGridStoreData.tsx
@@ -1,26 +1,26 @@
 const hamburgerGridStoreData = [
   {
     id: 0,
-    height: '28px',
-    width: '28px',
+    height: '24px',
+    width: '24px',
     src: '/images/gr4.svg',
   },
   {
     id: 1,
-    height: '28px',
-    width: '28px',
+    height: '24px',
+    width: '24px',
     src: '/images/gr3.svg',
   },
   {
     id: 2,
-    height: '28px',
-    width: '28px',
+    height: '24px',
+    width: '24px',
     src: '/images/gr2.svg',
   },
   {
     id: 3,
-    height: '28px',
-    width: '28px',
+    height: '24px',
+    width: '24px',
     src: '/images/gr.svg',
   },
 ]
