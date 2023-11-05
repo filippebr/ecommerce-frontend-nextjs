bb7ed1fbf001eb3b96d8843925ddd61f15b89707
Filippe Braga <filippeffx@hotmail.com>
2023-11-05 00:20:15 -0300

    size hamburger grid store view

--- src/data/hamburgerGridStoreData.tsx
+++ src/data/hamburgerGridStoreData.tsx
@@ -1,26 +1,26 @@
 const hamburgerGridStoreData = [
   {
     id: 0,
-    height: '20px',
-    width: '20px',
+    height: '30px',
+    width: '30px',
     src: '/images/gr4.svg',
   },
   {
     id: 1,
-    height: '20px',
-    width: '20px',
+    height: '30px',
+    width: '30px',
     src: '/images/gr3.svg',
   },
   {
     id: 2,
-    height: '20px',
-    width: '20px',
+    height: '30px',
+    width: '30px',
     src: '/images/gr2.svg',
   },
   {
     id: 3,
-    height: '20px',
-    width: '20px',
+    height: '30px',
+    width: '30px',
     src: '/images/gr.svg',
   },
 ]
