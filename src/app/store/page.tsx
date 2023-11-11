a04c46e8d9b87e102dc3266b29284a8b36198423
Filippe Braga <filippeffx@hotmail.com>
2023-11-10 23:28:21 -0300

    grid prop product card

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -68,7 +68,7 @@ export default function OurStore() {
               </div>
             </div>
             <div className="products-list pb-5">
-              <ProductCard />
+              <ProductCard gridProp={grid} />
             </div>
           </div>
         </div>
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -6,20 +6,21 @@ import ProductImage from './ProductImage'
 
 import { usePathname } from 'next/navigation'
 
-// interface ProductCardProps {
-//   gridProps: number
-// }
+interface ProductCardProps {
+  gridProp: number
+}
 
-export default function ProductCard() {
+export default function ProductCard({ gridProp }: ProductCardProps) {
   const path = usePathname()
 
   console.log(path === '/store')
-  // console.log(gridProps)
+  console.log(gridProp)
 
   return (
     <div className={`${path === '/store' ? `col-span-1` : 'col-span-3'}`}>
       <div className="group relative overflow-hidden rounded-lg bg-white p-4">
         {/* Wishlist */}
+        {gridProp}
         <div className="absolute right-5 top-0">
           <Link href={'/'}>
             <Image
