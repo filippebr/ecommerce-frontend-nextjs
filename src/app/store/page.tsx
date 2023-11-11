bc5e8e0d90b49471dc02ae6a5925bcea3aecbb43
Filippe Braga <filippeffx@hotmail.com>
2023-11-10 22:50:15 -0300

    removed comment rating star special product

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -68,7 +68,7 @@ export default function OurStore() {
               </div>
             </div>
             <div className="products-list pb-5">
-              <ProductCard />
+              <ProductCard gridProps={grid} />
             </div>
           </div>
         </div>
