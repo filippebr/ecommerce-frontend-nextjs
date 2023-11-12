3051d0e7814f186d6f55af93fa5fb4f8ba8abd83
Filippe Braga <filippeffx@hotmail.com>
2023-11-12 00:02:48 -0300

    nextjs update

--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -68,7 +68,7 @@ export default function OurStore() {
               </div>
             </div>
             <div className="products-list pb-5">
-              <ProductCard gridProp={grid} />
+              <ProductCard />
             </div>
           </div>
         </div>
