ca940e702ca96f4f0a3269d50092f800760d9121
Filippe Braga <filippeffx@hotmail.com>
2023-11-28 23:49:57 -0300

    col span 1 product card test

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -17,7 +17,8 @@ export default function ProductCard({ gridProp }: ProductCardProps) {
   console.log('gridProp: ', gridProp)
 
   return (
-    <div className={`${path === '/store' ? `col-span-1` : 'col-span-3'}`}>
+    // <div className={`${path === '/store' ? `col-span-1` : 'col-span-3'}`}>
+    <div className="col-span-1">
       <div className="group relative overflow-hidden rounded-lg bg-white p-4">
         {/* Wishlist */}
         {/* {gridProp} */}
