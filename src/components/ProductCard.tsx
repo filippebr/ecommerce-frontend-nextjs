d97616c11f27b0568e06bfbbb1319c7568ffb5d5
Filippe Braga <filippeffx@hotmail.com>
2023-11-29 21:18:39 -0300

    col span 6 product card component store view

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -17,9 +17,10 @@ export default function ProductCard({ gridProp }: ProductCardProps) {
   console.log('gridProp: ', gridProp)
 
   return (
-    <div
-      className={`${path === '/store' ? `col-span-${gridProp}` : 'col-span-3'}`}
-    >
+    // <div
+    //   className={`${path === '/store' ? `col-span-${gridProp}` : 'col-span-3'}`}
+    // >
+    <div className={`${path === '/store' ? `col-span-6` : 'col-span-3'}`}>
       <div className="group relative overflow-hidden rounded-lg bg-white p-4">
         {/* Wishlist */}
         <div className="absolute right-5 top-0">
